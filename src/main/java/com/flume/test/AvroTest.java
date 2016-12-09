d762c372cb46842c553911b2483129974181d862
kelong <ke.long@tcl.com>
2016-12-09 18:34:38 +0800

    roll file plugin

--- src/main/java/com/flume/test/AvroTest.java
+++ src/main/java/com/flume/test/AvroTest.java
@@ -12,13 +12,13 @@ public class AvroTest {
     public static void main(String[] args) {
         MyRpcClientFacade client = new MyRpcClientFacade();
         // Initialize client with the remote Flume agent's host and port
-        client.init("host.example.org", 41414);
+        client.init("localhost", 41414);
 
         // Send 10 events to the remote Flume agent. That agent should be
         // configured to listen with an AvroSource.
         String sampleData = "Hello Flume!";
-        for (int i = 0; i < 10; i++) {
-            client.sendDataToFlume(sampleData);
+        for (int i = 200; i < 1000; i++) {
+            client.sendDataToFlume(sampleData+i);
         }
 
         client.cleanUp();
