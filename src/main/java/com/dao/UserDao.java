506a9a2ddd2589144eb0f0c30c801734140511e6
longke <kelong_xhu@126.com>
2017-03-25 22:18:51 +0800

    page table

---
+++ src/main/java/com/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.dao;
+
+import com.dao.entity.User;
+
+public interface UserDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
