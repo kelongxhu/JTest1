9297607bff294bdc08b7bc217d01ab38e1a07731
ke.long <ke.long@56qq.com>
2017-09-20 17:35:13 +0800

    BeanCopierUtils

---
+++ src/main/java/com/dao/entity/User2.java
@@ -0,0 +1,85 @@
+package com.dao.entity;
+
+import java.util.Date;
+
+public class User2 {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String phone;
+
+    private Integer cityId;
+
+    private Date createTime;
+
+    private Date modifyTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Integer getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Integer cityId) {
+        this.cityId = cityId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+}
--- src/test/java/com/Test1.java
+++ src/test/java/com/Test1.java
@@ -1,8 +1,13 @@
 package com;
 
+import com.alibaba.fastjson.JSON;
+import com.dao.entity.User;
+import com.dao.entity.User2;
 import com.redis.A;
+import com.util.BeanCopierUtils;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.BeanUtils;
 
 import java.util.UUID;
 import java.util.regex.Matcher;
@@ -23,18 +28,20 @@ public class Test1 {
         //        assertEquals(5, a.add(2, 3));
 
         String s =
-            "58.215.204.118 - - [18/Sep/2013:06:51:35 +0000] \"GET /nodejs-socketio-chat/ HTTP/1.1\" 200 10818 \"http://www.google.com/url?sa=t&rct=j&q=nodejs%20%E5%BC%82%E6%AD%A5%E5%B9%BF%E6%92%AD&source=web&cd=1&cad=rja&ved=0CCgQFjAA&url=%68%74%74%70%3a%2f%2f%62%6c%6f%67%2e%66%65%6e%73%2e%6d%65%2f%6e%6f%64%65%6a%73%2d%73%6f%63%6b%65%74%69%6f%2d%63%68%61%74%2f&ei=rko5UrylAefOiAe7_IGQBw&usg=AFQjCNG6YWoZsJ_bSj8kTnMHcH51hYQkAA&bvm=bv.52288139,d.aGc\" \"Mozilla/5.0 (Windows NT 5.1; rv:23.0) Gecko/20100101 Firefox/23.0\"";
+                "58.215.204.118 - - [18/Sep/2013:06:51:35 +0000] \"GET /nodejs-socketio-chat/ HTTP/1.1\" 200 10818 \"http://www.google"
+                        + ".com/url?sa=t&rct=j&q=nodejs%20%E5%BC%82%E6%AD%A5%E5%B9%BF%E6%92%AD&source=web&cd=1&cad=rja&ved=0CCgQFjAA&url"
+                        + "=%68%74%74%70%3a%2f%2f%62%6c%6f%67%2e%66%65%6e%73%2e%6d%65%2f%6e%6f%64%65%6a%73%2d%73%6f%63%6b%65%74%69%6f%2d"
+                        + "%63%68%61%74%2f&ei=rko5UrylAefOiAe7_IGQBw&usg=AFQjCNG6YWoZsJ_bSj8kTnMHcH51hYQkAA&bvm=bv.52288139,d.aGc\" "
+                        + "\"Mozilla/5.0 (Windows NT 5.1; rv:23.0) Gecko/20100101 Firefox/23.0\"";
         String[] arr = s.split(" ");
         for (String i : arr) {
             System.out.println("value:" + i);
         }
 
-
         System.out.println("UUID:" + UUID.randomUUID().toString().replaceAll("-", ""));
 
         System.out.println(isContainChinese("china123是"));
     }
-
 
     public static boolean isContainChinese(String str) {
 
@@ -51,7 +58,6 @@ public class Test1 {
         handle("tableName", "dataSourceKey", 100, 2, 10);
     }
 
-
     /**
      * 分库分表获取DB,TABLE位置
      *
@@ -67,12 +73,28 @@ public class Test1 {
         long tablePosition = shard_value % tableShardNum;
         long dbPosition = tablePosition / (tableShardNum / dbShardNum);
         String finalTableName =
-            new StringBuilder().append(tableName).append("_").append(tablePosition).toString();
+                new StringBuilder().append(tableName).append("_").append(tablePosition).toString();
         String finalDataSourceKey =
-            new StringBuilder().append(dataSourceKey).append(dbPosition).toString();
+                new StringBuilder().append(dataSourceKey).append(dbPosition).toString();
 
         System.out.println(finalTableName);
         System.out.println(finalDataSourceKey);
+    }
+
+    @Test
+    public void copyTest() {
+        User user = new User();
+        user.setUsername("test");
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i++) {
+            User2 user2 = new User2();
+            //BeanCopierUtils.copyProperties(user, user2); //69
+            BeanUtils.copyProperties(user,user2); //414
+        }
+        System.out.println("costs:" + (System.currentTimeMillis() - start));
+        //System.out.println(JSON.toJSONString(user));
+        //System.out.println(JSON.toJSONString(user2));
     }
 
 }
